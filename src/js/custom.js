4337330a52976b397f7a0aa224860c051ae1dfef
unknown <andersondiassiqueira33@Outlook.com>
2019-11-12 17:12:22 -0300

    Content

---
+++ src/js/custom.js
@@ -0,0 +1,32 @@
+jQuery('#ideathon').slick({
+    // centerMode: true,
+    // centerPadding: '60px',
+    slidesToShow: 1,
+    slidesToScroll:1,
+    infinite: false,
+    swipe: false
+    // responsive: [
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 3
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 1
+    //     }
+    //   }
+    // ]
+ 
+  });
+ 
+  
+ 
