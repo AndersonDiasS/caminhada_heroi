0ddb013462a233976fbfc6282b33acf64d158d55
unknown <andersondiassiqueira33@Outlook.com>
2019-11-18 16:06:44 -0300

    Estrutura html

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,10 +1,13 @@
-jQuery('#ideathon').slick({
-    // centerMode: true,
+// #ideathon - id primeiro swiper
+jQuery('.swiper').slick({
+    centerMode: true,
     // centerPadding: '60px',
     slidesToShow: 1,
     slidesToScroll:1,
     infinite: false,
-    swipe: false
+    swipe: false,
+    prevArrow: $(".btn-prev"),
+    nextArrow: $(".btn-next"),
     // responsive: [
     //   {
     //     breakpoint: 768,
@@ -28,5 +31,24 @@ jQuery('#ideathon').slick({
  
   });
  
+  // Scrolling - Parametros 
+
+
+  $( window ).ready(function() {
   
- 
+    var wHeight = $(window).height();
+
+    $('.change')
+      .height(wHeight)
+      .scrollie({
+        scrollOffset : -50,
+        scrollingInView : function(elem) {
+                   
+          var bgColor = elem.data('background');
+          
+          $('.scrolling-content').css('background', bgColor);
+          
+        }
+      });
+
+  });
