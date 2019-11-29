520a57758f0b8a713d97b4a3c6319bfa2d4327b6
unknown <andersondiassiqueira33@Outlook.com>
2019-11-29 17:31:26 -0300

    Resp - 760 a 440

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,25 +1,35 @@
 new WOW().init();
 // Leagues 
 var sliders = {
-  1: {slider: '#ideathon', navprev: '.navideathonprev', navnext: '.navideathonnext'},
-  2: {slider: '#Leaners' , navprev: '.navLeanersprev',  navnext: '.navLeanersnext' },
-  3: {slider: '#Builders', navprev: '.navBuildersprev', navnext: '.navBuildersnext'},
-  4: {slider: '#Players' , navprev: '.navPlayersprev', navnext: '.navPlayersnext'},
-  5: {slider: '#Justice' , navprev: '.navJusticeprev', navnext: '.navJusticenext'}
+  1: { slider: '#ideathon', navprev: '.navideathonprev', navnext: '.navideathonnext' },
+  2: { slider: '#Leaners', navprev: '.navLeanersprev', navnext: '.navLeanersnext' },
+  3: { slider: '#Builders', navprev: '.navBuildersprev', navnext: '.navBuildersnext' },
+  4: { slider: '#Players', navprev: '.navPlayersprev', navnext: '.navPlayersnext' },
+  5: { slider: '#Justice', navprev: '.navJusticeprev', navnext: '.navJusticenext' }
 };
 
-$.each(sliders, function(){
+$.each(sliders, function () {
   $(this.slider).slick({
     centerMode: true,
     slidesToShow: 1,
-    slidesToScroll:1,
+    slidesToScroll: 1,
     infinite: false,
     swipe: false,
     // Colocar em um array
     prevArrow: (this.navprev),
-    nextArrow: (this.navnext), 
-  });
+    nextArrow: (this.navnext),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          centerMode: false
+        }
+      },
  
+      
+    ]
+  });
+
 });
 
 // TEXT SWIPER 
@@ -43,74 +53,85 @@ $('.swiper-helper').slick({
         arrows: false
       }
     }
-     
-    ]
+
+  ]
 
 });
 
 
 
-  
-  // Scrolling - Parametros 
+
+// Scrolling - Parametros 
 
 
-  $( window ).ready(function() {
-  
-    var wHeight = $(window).height();
+$(window).ready(function () {
 
-    $('.change')
-      .height(wHeight)
-      .scrollie({
-        scrollOffset : -50,
-        scrollingInView : function(elem) {
-                   
-          var bgColor = elem.data('background');
-          
-          $('.scrolling-content').css('background', bgColor);
-          
-        }
-      });
+  var wHeight = $(window).height();
 
-  });
- 
- 
-  function change(step){
-    switch(step) {
-      case 0:
-        // alert(0);
-        $('.div-close').show("slow"),
+  $('.change')
+    .height(wHeight)
+    .scrollie({
+      scrollOffset: -50,
+      scrollingInView: function (elem) {
+
+        var bgColor = elem.data('background');
+
+        $('.scrolling-content').css('background', bgColor);
+
+      }
+    });
+
+});
+
+
+function change(step) {
+
+  switch (step) {
+    case 0:
+      // alert(0);
+      $('.div-close').show("slow"),
         $('.div-open').hide('slow'),
-        $('.div-open1').hide('slow');  
-        $('.div-open2').hide('slow');  
-        break;
-      case 1:
-        // alert(1);
-        $('.div-close').hide("slow"),
+        $('.div-open1').hide('slow');
+      $('.div-open2').hide('slow');
+
+      break;
+    case 1:
+      // alert(1);
+      $('.div-close').hide("slow"),
         $('.div-open').show('slow'),
-        $('.div-open1').hide('slow');  
-        $('.div-open2').hide('slow');  
-        break;
-      case 2:
-        // alert(2);
-        $('.div-open').hide('slow'), 
-        $('.div-close').hide('slow'),  
-        $('.div-open1').show('slow');  
-        $('.div-open2').hide('slow');  
-        break;
-        case 3:
-          // alert(3);
-          $('.div-open').hide('slow'), 
-          $('.div-close').hide('slow'),  
-          $('.div-open1').hide('slow');  
-          $('.div-open2').show('slow');  
-          break;
-      default:
-        break;      
-    }
+        $('.div-open1').hide('slow');
+      $('.div-open2').hide('slow');
+
+      break;
+    case 2:
+      // alert(2);
+      $('.div-open').hide('slow'),
+        $('.div-close').hide('slow'),
+        $('.div-open1').show('slow');
+      $('.div-open2').hide('slow');
+
+      break;
+    case 3:
+      // alert(3);
+      $('.div-open').hide('slow'),
+        $('.div-close').hide('slow'),
+        $('.div-open1').hide('slow');
+      $('.div-open2').show('slow');
+
+      break;
+    case 4:
+      $('.div-open').hide('slow'),
+        $('.div-close').hide('slow'),
+        $('.div-open1').hide('slow');
+      $('.div-open2').hide('slow');
+
+    default:
+      break;
   }
+}
 
 
- 
+
 // #Parallax
 
 var rellax = new Rellax('.rellax');
